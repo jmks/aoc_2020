79fa6ac77a3903fe6959af276a8a03c3fe9e1c4b
Jason Schweier <jasons@nulogy.com>
2020-12-02 10:52:05 -0500

    Day 2, part 2

--- test/aoc2020/day_02_password_philosophy_test.exs
+++ test/aoc2020/day_02_password_philosophy_test.exs
@@ -3,20 +3,33 @@ defmodule Aoc2020.Day02PasswordPhilosophyTest do
 
   import Aoc2020.Day02PasswordPhilosophy
 
-  test "valid passwords" do
-    assert validate_password("1-3 a: abcde")
-    refute validate_password("1-3 b: cdefg")
-    assert validate_password("2-9 c: ccccccccc")
+  test "valid passwords by count in range" do
+    assert validate_password(:letter_count, "1-3 a: abcde")
+    refute validate_password(:letter_count, "1-3 b: cdefg")
+    assert validate_password(:letter_count, "2-9 c: ccccccccc")
   end
 
   test "count_valid" do
-    assert count_valid(["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]) == 2
+    assert count_valid(:letter_count, ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"]) == 2
   end
 
   test "solve part 1" do
-    count = Input.strings(2) |> count_valid
+    count = count_valid(:letter_count, Input.strings(2))
 
     IO.puts("")
     IO.puts("Part 1: #{count}")
+  end
+
+  test "valid passwords by position" do
+    assert validate_password(:letter_positions, "1-3 a: abcde")
+    refute validate_password(:letter_positions, "1-3 b: cdefg")
+    refute validate_password(:letter_positions, "2-9 c: ccccccccc")
+  end
+
+  test "solve part 2" do
+    count = count_valid(:letter_positions, Input.strings(2))
+
+    IO.puts("")
+    IO.puts("Part 2: #{count}")
   end
 end
