1f982c558999e69dd5b289e91e436f609964ea93
Jason Schweier <jasons@nulogy.com>
2020-12-05 16:11:31 -0500

    Day 5, part 2

--- test/aoc2020/day_05_binary_boarding_test.exs
+++ test/aoc2020/day_05_binary_boarding_test.exs
@@ -16,4 +16,21 @@ defmodule Aoc2020.Day05BinaryBoardingTest do
     IO.puts("")
     IO.puts("Part 1: #{result}")
   end
+
+  test "solve part 2" do
+    id =
+      Input.strings(5)
+      |> Enum.map(fn code -> code |> seat |> seat_id end)
+      |> Enum.sort
+      |> Enum.reduce(fn current, previous ->
+        if previous + 1 == current do
+          current
+        else
+          previous
+        end
+      end)
+
+    IO.puts("")
+    IO.puts("Part 2: #{id + 1}")
+  end
 end
