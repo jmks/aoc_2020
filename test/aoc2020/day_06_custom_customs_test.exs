33971624ea13a2e29affacf51f114f799c8b32f7
Jason Schweier <jasons@nulogy.com>
2020-12-06 19:25:24 -0500

    Day 6, part 2

--- test/aoc2020/day_06_custom_customs_test.exs
+++ test/aoc2020/day_06_custom_customs_test.exs
@@ -25,14 +25,25 @@ defmodule Aoc2020.Day06CustomCustomsTest do
     assert length(groups(@test)) == 5
   end
 
-  test "yeses" do
-    assert sum_of_group_yes(@test) == 11
+  test "any yes" do
+    assert sum_of_any_yes(@test) == 11
   end
 
   test "solve part 1" do
-    result = sum_of_group_yes(Input.strings(6))
+    result = sum_of_any_yes(Input.strings(6))
 
     IO.puts("")
     IO.puts("Part 1: #{result}")
+  end
+
+  test "all yes" do
+    assert sum_of_all_yes(@test) == 6
+  end
+
+  test "solve part 2" do
+    result = sum_of_all_yes(Input.strings(6))
+
+    IO.puts("")
+    IO.puts("Part 2: #{result}")
   end
 end
