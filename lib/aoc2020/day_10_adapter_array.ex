77e7fa468f5ac771763b0c579ec8f82dbe708562
Jason Schweier <jasons@nulogy.com>
2020-12-10 15:09:23 -0500

    Day 10, part 2

--- lib/aoc2020/day_10_adapter_array.ex
+++ lib/aoc2020/day_10_adapter_array.ex
@@ -78,9 +78,64 @@ Here is a larger example:
 34
 10
 3
+
 In this larger example, in a chain that uses all of the adapters, there are 22 differences of 1 jolt and 10 differences of 3 jolts.
 
 Find a chain that uses all of your adapters to connect the charging outlet to your device's built-in adapter and count the joltage differences between the charging outlet, the adapters, and your device. What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
+
+--- Part Two ---
+To completely determine whether you have enough adapters, you'll need to figure out how many different ways they can be arranged. Every arrangement needs to connect the charging outlet to your device. The previous rules about when adapters can successfully connect still apply.
+
+The first example above (the one that starts with 16, 10, 15) supports the following arrangements:
+
+(0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+
+(The charging outlet and your device's built-in adapter are shown in parentheses.) Given the adapters from the first example, the total number of arrangements that connect the charging outlet to your device is 8.
+
+The second example above (the one that starts with 28, 33, 18) has many arrangements. Here are a few:
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 48, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 46, 49, (52)
+
+(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+32, 33, 34, 35, 38, 39, 42, 45, 47, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+46, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+46, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+47, 48, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+47, 49, (52)
+
+(0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+48, 49, (52)
+
+In total, this set of adapters can connect the charging outlet to your device in 19208 distinct arrangements.
+
+You glance back down at your bag and try to remember why you brought so many adapters; there must be more than a trillion valid ways to arrange them! Surely, there must be an efficient way to count the arrangements.
+
+What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?
 """
   def joltage_difference_frequencies(adapters) do
     [0 | adapters]
@@ -99,4 +154,35 @@ Find a chain that uses all of your adapters to connect the charging outlet to yo
 
   def generate_pairs([_ | []]), do: []
   def generate_pairs([x, y | rest]), do: [{x, y} | generate_pairs([y | rest])]
+
+  def adapter_paths(adapters) do
+    source = 0
+    adapters_from_source = Enum.sort([source | adapters])
+    next_adapters = next_adapters(adapters_from_source)
+
+    [last | reversed] = Enum.reverse(adapters_from_source)
+
+    path_tails =
+      reversed
+      |> Enum.reduce(%{last => 1}, fn adapter, tails ->
+        count =
+          Map.get(next_adapters, adapter)
+          |> Enum.map(&Map.get(tails, &1, 1))
+          |> Enum.sum
+
+        Map.put(tails, adapter, count)
+      end)
+
+    Map.get(path_tails, source)
+  end
+
+  defp next_adapters(adapters), do: do_next_adapters(adapters, %{})
+
+  defp do_next_adapters([x], map), do: Map.put(map, x, [])
+  defp do_next_adapters([adapter | adapters], map) do
+    next = Enum.take_while(adapters, fn a -> adapter + 3 >= a end)
+    new_map = Map.put(map, adapter, next)
+
+    do_next_adapters(adapters, new_map)
+  end
 end
