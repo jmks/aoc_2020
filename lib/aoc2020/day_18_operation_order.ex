ed8ab568bc57e23d5ff323634eadcbf485f7722f
Jason Schweier <jasons@nulogy.com>
2020-12-21 13:33:06 -0500

    Day 18, part 2

--- lib/aoc2020/day_18_operation_order.ex
+++ lib/aoc2020/day_18_operation_order.ex
@@ -34,53 +34,136 @@ Here are a few more examples:
 ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 becomes 13632.
 
 Before you can help with the homework, you need to understand it yourself. Evaluate the expression on each line of the homework; what is the sum of the resulting values?
+
+--- Part Two ---
+
+You manage to answer the child's questions and they finish part 1 of their homework, but get stuck when they reach the next section: advanced math.
+
+Now, addition and multiplication have different precedence levels, but they're not the ones you're familiar with. Instead, addition is evaluated before multiplication.
+
+For example, the steps to evaluate the expression 1 + 2 * 3 + 4 * 5 + 6 are now as follows:
+
+1 + 2 * 3 + 4 * 5 + 6
+3   * 3 + 4 * 5 + 6
+3   *   7   * 5 + 6
+3   *   7   *  11
+21       *  11
+231
+
+Here are the other examples from above:
+
+1 + (2 * 3) + (4 * (5 + 6)) still becomes 51.
+2 * 3 + (4 * 5) becomes 46.
+5 + (8 * 3 + 9 + 3 * 4 * 3) becomes 1445.
+5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)) becomes 669060.
+((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 becomes 23340.
+
+What do you get if you add up the results of evaluating the homework problems using these new rules?
 """
   def parse(homework) do
     homework
     |> String.graphemes
     |> Enum.filter(&(&1 != " "))
-    |> do_parse(nil)
+    |> do_parse([])
   end
 
-  def eval(num) when is_integer(num), do: num
-  def eval({:add, lhs, rhs}), do: eval(lhs) + eval(rhs)
-  def eval({:mul, lhs, rhs}), do: eval(lhs) * eval(rhs)
+  def eval(tokens) do
+    priorities = %{
+      parens: 1,
+      mul: 2,
+      add: 2,
+      num: 4
+    }
 
-  defp do_parse([], expr), do: expr
+    evaluate_with_priorities(tokens, priorities)
+  end
 
-  defp do_parse(["+" | rest], {lhs}), do: do_parse(rest, {:add, lhs})
-  defp do_parse(["+" | rest], lhs), do: do_parse(rest, {:add, lhs})
+  def eval2(tokens) do
+    priorities = %{
+      parens: 1,
+      add: 2,
+      mul: 3,
+      num: 4
+    }
 
-  defp do_parse(["*" | rest], {lhs}), do: do_parse(rest, {:mul, lhs})
-  defp do_parse(["*" | rest], lhs), do: do_parse(rest, {:mul, lhs})
+    evaluate_with_priorities(tokens, priorities)
+  end
 
-  defp do_parse(["(" | rest], nil) do
+  def evaluate_with_priorities([result], _) when is_integer(result), do: result
+
+  def evaluate_with_priorities([left, :mul, right], _) when is_integer(left) and is_integer(right) do
+    left * right
+  end
+
+  def evaluate_with_priorities([left, :add, right], _) when is_integer(left) and is_integer(right) do
+    left + right
+  end
+
+  def evaluate_with_priorities({:parens, exprs}, priorities), do: evaluate_with_priorities(exprs, priorities)
+
+  def evaluate_with_priorities(tokens, priorities) do
+    {leading, next_expr, trailing} = find_next(tokens, priorities)
+    evaluate_with_priorities(leading ++ [evaluate_with_priorities(next_expr, priorities)] ++ trailing, priorities)
+  end
+
+  defp do_parse([], tokens), do: Enum.reverse(tokens)
+
+  defp do_parse(["+" | rest], tokens), do: do_parse(rest, [:add | tokens])
+
+  defp do_parse(["*" | rest], tokens), do: do_parse(rest, [:mul | tokens])
+
+  defp do_parse(["(" | rest], tokens) do
     {sub, rest} = subexpr(rest, [], 1)
-    expr = do_parse(sub, nil)
+    expr = do_parse(sub, [])
 
-    case rest do
-      [] -> expr
-      more -> do_parse(more, {expr})
-    end
+    do_parse(rest, [{:parens, expr} | tokens])
   end
 
-  defp do_parse(["(" | rest], {op, lhs}) do
-    {sub, rest} = subexpr(rest, [], 1)
-    expr = do_parse(sub, nil)
-
-    do_parse(rest, {op, lhs, expr})
-  end
-
-  defp do_parse([number | rest], {op, lhs}) do
-    do_parse(rest, {op, lhs, String.to_integer(number)})
-  end
-
-  defp do_parse([number | rest], nil) do
-    do_parse(rest, {String.to_integer(number)})
+  defp do_parse([number | rest], tokens) do
+    do_parse(rest, [String.to_integer(number) | tokens])
   end
 
   defp subexpr([")" | rest], chars,     1), do: {Enum.reverse(chars), rest}
   defp subexpr([")" | rest], chars, level), do: subexpr(rest, [")" | chars], level - 1)
   defp subexpr(["(" | rest], chars, level), do: subexpr(rest, ["(" | chars], level + 1)
   defp subexpr([ch  | rest], chars, level), do: subexpr(rest, [ch | chars], level)
+
+  defp find_next(tokens, priorities) do
+    next_range =
+      tokens
+      |> prioritize(priorities)
+      |> hd
+      |> elem(1)
+
+    case next_range do
+      0..max ->
+        leading = []
+        next = Enum.take(tokens, max+1)
+        trailing = Enum.drop(tokens, max+1)
+
+        {leading, next, trailing}
+
+      min..max ->
+        leading = Enum.take(tokens, min)
+        next = tokens |> Enum.drop(min) |> Enum.take(max - min + 1)
+        trailing = Enum.drop(tokens, max+1)
+
+        {leading, next, trailing}
+
+      index ->
+        {Enum.take(tokens, index), Enum.at(tokens, index), Enum.drop(tokens, index+1)}
+    end
+  end
+
+  defp prioritize(tokens, priorities) do
+    tokens
+    |> Enum.with_index
+    |> Enum.map(fn
+      {{:parens, _}, index} -> {Map.get(priorities, :parens), index}
+      {:add, index} -> {Map.get(priorities, :add), (index-1)..(index+1)}
+      {:mul, index} -> {Map.get(priorities, :mul), (index-1)..(index+1)}
+      {_, index} -> {Map.get(priorities, :num), index}
+    end)
+    |> Enum.sort_by(fn {p, _} -> p end)
+  end
 end
--- test/aoc2020/day_18_operation_order_test.exs
+++ test/aoc2020/day_18_operation_order_test.exs
@@ -4,14 +4,18 @@ defmodule Aoc2020.Day18OperationOrderTest do
   import Aoc2020.Day18OperationOrder
 
   test "parsing" do
-    assert parse("1 + 2 * 3 + 4 * 5 + 6") == {:add, {:mul, {:add, {:mul, {:add, 1, 2}, 3}, 4}, 5}, 6}
-    assert parse("1 + (2 * 3) + (4 * (5 + 6))") == {:add, {:add, 1, {:mul, 2, 3}}, {:mul, 4, {:add, 5, 6}}}
+    assert parse("1 + 2 * 3 + 4 * 5 + 6") == [1, :add, 2, :mul, 3, :add, 4, :mul, 5, :add, 6]
+    assert parse("1 + (2 * 3) + (4 * (5 + 6))") == [
+      1,
+      :add,
+      {:parens, [2, :mul, 3]},
+      :add,
+      {:parens, [4, :mul, {:parens, [5, :add, 6]}]}
+    ]
   end
 
   test "evaluating" do
-    assert eval({:add, {:mul, {:add, {:mul, {:add, 1, 2}, 3}, 4}, 5}, 6}) == 71
-    assert eval({:add, {:add, 1, {:mul, 2, 3}}, {:mul, 4, {:add, 5, 6}}}) == 51
-
+    assert ("1 + 2 * 3 + 4 * 5 + 6" |> parse |> eval) == 71
     assert ("2 * 3 + (4 * 5)" |> parse |> eval) == 26
     assert ("5 + (8 * 3 + 9 + 3 * 4 * 3)" |> parse |> eval) == 437
     assert ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" |> parse |> eval) == 12240
@@ -27,5 +31,23 @@ defmodule Aoc2020.Day18OperationOrderTest do
 
     IO.puts("")
     IO.puts("Part 1: #{result}")
+  end
+
+  test "multiplication first" do
+    assert ("2 * 3 + (4 * 5)" |> parse |> eval2) == 46
+    assert ("5 + (8 * 3 + 9 + 3 * 4 * 3)" |> parse |> eval2) == 1445
+    assert ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))" |> parse |> eval2) == 669060
+    assert ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2" |> parse |> eval2) == 23340
+  end
+
+  test "part 2" do
+    result =
+      Input.strings(18)
+      |> Enum.map(&parse/1)
+      |> Enum.map(&eval2/1)
+      |> Enum.reduce(0, &Kernel.+/2)
+
+    IO.puts("")
+    IO.puts("Part 2: #{result}")
   end
 end
