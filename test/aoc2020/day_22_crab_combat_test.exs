a7d5b384fc3f1d797a93f14be30d2bd52d39bc95
Jason Schweier <jasons@nulogy.com>
2021-01-04 18:27:46 -0500

    Day 22, part 2

--- test/aoc2020/day_22_crab_combat_test.exs
+++ test/aoc2020/day_22_crab_combat_test.exs
@@ -5,28 +5,39 @@ defmodule Aoc2020.Day22CrabCombatTest do
   alias Aoc2020.Day22CrabCombat.Combat
 
   test "plays combat" do
-    combat = Combat.new([9, 2, 6, 3, 1], [5, 8, 4, 7, 10]) |> run_until_done
+    combat = Combat.new([9, 2, 6, 3, 1], [5, 8, 4, 7, 10]) |> Combat.play_until_done
 
     assert Combat.score(combat) == 306
   end
 
   test "part 1" do
-    {one, two} = puzzle
+    {one, two} = puzzle()
 
-    result = Combat.new(one, two) |> run_until_done |> Combat.score
+    result = Combat.new(one, two) |> Combat.play_until_done |> Combat.score
 
     IO.puts("")
     IO.puts("Part 1: #{result}")
   end
 
-  defp run_until_done(combat) do
-    case Combat.round(combat) do
-      {:ok, new_combat} ->
-        run_until_done(new_combat)
+  test "plays recursive combat" do
+    combat = Combat.new([9, 2, 6, 3, 1], [5, 8, 4, 7, 10], :recursive) |> Combat.play_until_done
 
-      {:game_over, done} ->
-        done
-    end
+    assert Combat.score(combat) == 291
+  end
+
+  test "game does not loop forever" do
+    combat = Combat.new([43, 19], [2, 29, 14], :recursive) |> Combat.play_until_done
+
+    assert Combat.done?(combat)
+  end
+
+  test "part 2" do
+    {one, two} = puzzle()
+
+    result = Combat.new(one, two, :recursive) |> Combat.play_until_done |> Combat.score
+
+    IO.puts("")
+    IO.puts("Part 2: #{result}")
   end
 
   defp puzzle do
