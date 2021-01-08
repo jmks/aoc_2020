20e229fcebfaa38dfa6453d07c6b2e11bbaf7a0d
Jason Schweier <jasons@nulogy.com>
2021-01-07 19:03:36 -0500

    Day 24, part 2

--- lib/aoc2020/day_24_lobby_layout.ex
+++ lib/aoc2020/day_24_lobby_layout.ex
@@ -41,6 +41,43 @@ defmodule Aoc2020.Day24LobbyLayout do
   In the above example, 10 tiles are flipped once (to black), and 5 more are flipped twice (to black, then back to white). After all of these instructions have been followed, a total of 10 tiles are black.
 
   Go through the renovation crew's list and determine which tiles they need to flip. After all of the instructions have been followed, how many tiles are left with the black side up?
+
+  --- Part Two ---
+
+  The tile floor in the lobby is meant to be a living art exhibit. Every day, the tiles are all flipped according to the following rules:
+
+  Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+  Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+  Here, tiles immediately adjacent means the six tiles directly touching the tile in question.
+
+  The rules are applied simultaneously to every tile; put another way, it is first determined which tiles need to be flipped, then they are all flipped at the same time.
+
+  In the above example, the number of black tiles that are facing up after the given number of days has passed is as follows:
+
+  Day 1: 15
+  Day 2: 12
+  Day 3: 25
+  Day 4: 14
+  Day 5: 23
+  Day 6: 28
+  Day 7: 41
+  Day 8: 37
+  Day 9: 49
+  Day 10: 37
+
+  Day 20: 132
+  Day 30: 259
+  Day 40: 406
+  Day 50: 566
+  Day 60: 788
+  Day 70: 1106
+  Day 80: 1373
+  Day 90: 1844
+  Day 100: 2208
+
+  After executing this process a total of 100 times, there would be 2208 black tiles facing up.
+
+  How many tiles will be black after 100 days?
   """
   defmodule HexagonalGrid do
     defstruct [:tiles]
@@ -51,39 +88,77 @@ defmodule Aoc2020.Day24LobbyLayout do
 
     def flip(grid, path) do
       tile_coordinate = travel(path, {0, 0})
-      colour = Map.get(grid.tiles, tile_coordinate, :white)
+      colour = Map.get(grid.tiles, tile_coordinate, :white) |> flip
 
-      %{grid | tiles: Map.put(grid.tiles, tile_coordinate, flip(colour))}
+      new_tiles =
+        case colour do
+          :white -> Map.delete(grid.tiles, tile_coordinate)
+          :black -> Map.put(grid.tiles, tile_coordinate, :black)
+        end
+
+      %{grid | tiles: new_tiles}
     end
 
     def black_tiles(grid) do
-      grid.tiles
-      |> Map.values
-      |> Enum.filter(fn
-        :white -> false
-        :black -> true
-      end)
-      |> length
+      map_size(grid.tiles)
     end
 
     def parse_path(path) do
       do_parse_path(path, [])
     end
 
+    def next_day(grid) do
+      new_tiles =
+        grid.tiles
+        |> Enum.flat_map(fn {tile, :black} ->
+          black = black_tiles_around(tile, grid.tiles)
+          new_colour = if black == 0 or black > 2, do: :white, else: :black
+
+          adjacent_colours = Enum.map(adjacent(tile), fn tile ->
+            colour = if black_tiles_around(tile, grid.tiles) == 2, do: :black, else: :white
+
+            {tile, colour}
+          end)
+
+          [{tile, new_colour} | adjacent_colours]
+        end)
+        |> Enum.filter(fn
+          {_, :black} -> true
+          ___________ -> false
+        end)
+        |> Enum.into(%{})
+
+      %{grid | tiles: new_tiles}
+    end
+
+    defp adjacent(coordinate) do
+      ~w(e se ne w sw nw) |> Enum.map(&move(coordinate, &1))
+    end
+
+    defp move({q, r}, direction) do
+      {dq, dr} = offsets(direction)
+
+      {q + dq, r + dr}
+    end
+
+    defp black_tiles_around(coordinate, tiles) do
+      coordinate
+      |> adjacent
+      |> Enum.map(&(Map.get(tiles, &1)))
+      |> Enum.filter(&(&1 == :black))
+      |> length
+    end
+
     defp travel(path, coordinate) do
       path
       |> parse_path
-      |> Enum.reduce(coordinate, fn direction, {west, south} ->
-        {dw, ds} = offsets(direction)
-
-        {west + dw, south + ds}
-      end)
+      |> Enum.reduce(coordinate, &move(&2, &1))
     end
 
-    defp offsets("e"),  do: {1, 0}
+    defp offsets("e"), do: {1, 0}
     defp offsets("se"), do: {0, 1}
     defp offsets("ne"), do: {1, -1}
-    defp offsets("w"),  do: {-1, 0}
+    defp offsets("w"), do: {-1, 0}
     defp offsets("sw"), do: {-1, 1}
     defp offsets("nw"), do: {0, -1}
 
@@ -91,10 +166,10 @@ defmodule Aoc2020.Day24LobbyLayout do
     defp flip(:black), do: :white
 
     defp do_parse_path("", path), do: Enum.reverse(path)
-    defp do_parse_path("e" <> rest, path),  do: do_parse_path(rest, ["e"  | path])
+    defp do_parse_path("e" <> rest, path), do: do_parse_path(rest, ["e" | path])
     defp do_parse_path("se" <> rest, path), do: do_parse_path(rest, ["se" | path])
     defp do_parse_path("sw" <> rest, path), do: do_parse_path(rest, ["sw" | path])
-    defp do_parse_path("w" <> rest, path),  do: do_parse_path(rest, ["w"  | path])
+    defp do_parse_path("w" <> rest, path), do: do_parse_path(rest, ["w" | path])
     defp do_parse_path("nw" <> rest, path), do: do_parse_path(rest, ["nw" | path])
     defp do_parse_path("ne" <> rest, path), do: do_parse_path(rest, ["ne" | path])
   end
--- test/aoc2020/day_24_lobby_layout_test.exs
+++ test/aoc2020/day_24_lobby_layout_test.exs
@@ -32,20 +32,48 @@ defmodule Aoc2020.Day24LobbyLayoutTest do
   end
 
   test "example" do
-    outer_grid = HexagonalGrid.new
-
-    assert Enum.reduce(@test, outer_grid, fn tile, grid -> HexagonalGrid.flip(grid, tile) end) |> HexagonalGrid.black_tiles() == 10
+    assert flip_tiles(HexagonalGrid.new, @test) |> HexagonalGrid.black_tiles() == 10
   end
 
   test "part 1" do
     result =
-      Input.strings(24)
-      |> Enum.reduce(HexagonalGrid.new, fn tile, grid ->
-        HexagonalGrid.flip(grid, tile)
-      end)
+      flip_tiles(HexagonalGrid.new, Input.strings(24))
       |> HexagonalGrid.black_tiles
 
     IO.puts("")
     IO.puts("Part 1: #{result}")
+  end
+
+  test "living tiles" do
+    zero = flip_tiles(HexagonalGrid.new, @test)
+
+    assert flip_days(zero, 1) |> HexagonalGrid.black_tiles == 15
+    assert flip_days(zero, 2) |> HexagonalGrid.black_tiles == 12
+    assert flip_days(zero, 3) |> HexagonalGrid.black_tiles == 25
+    assert flip_days(zero, 20) |> HexagonalGrid.black_tiles == 132
+    assert flip_days(zero, 100) |> HexagonalGrid.black_tiles == 2208
+  end
+
+  test "part 2" do
+    result =
+      flip_tiles(HexagonalGrid.new, Input.strings(24))
+      |> flip_days(100)
+      |> HexagonalGrid.black_tiles
+
+    IO.puts("")
+    IO.puts("Part 2: #{result}")
+  end
+
+  defp flip_tiles(grid, tiles) do
+    Enum.reduce(tiles, grid, fn tile, grid ->
+      HexagonalGrid.flip(grid, tile)
+    end)
+  end
+
+  defp flip_days(grid, 0), do: grid
+  defp flip_days(grid, days) do
+    grid
+    |> HexagonalGrid.next_day
+    |> flip_days(days - 1)
   end
 end
